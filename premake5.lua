1b273e86880ebc132ed1bddd8870249acb1706f3
Bartłomiej Postek <68648068+BarPos@users.noreply.github.com>
2023-02-23 14:14:42 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -2,20 +2,24 @@ project "ImGui"
 	kind "StaticLib"
 	language "C++"
 
-  targetdir ("%{wks.location}/build/" .. outputdir .. "/%{prj.name}")
-	debugdir ("%{wks.location}/build/" .. outputdir .. "/%{prj.name}")
-	objdir ("%{wks.location}/build/tmp/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin/tmp/" .. outputdir .. "/%{prj.name}")
 
-  files
+	files
 	{
-    "imgui.cpp",
-    "imgui_draw.cpp",
-    "imgui_demo.cpp",
-    "imgui_tables.cpp",
-    "imgui_widgets.cpp"
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
 	}
 
-filter "system:windows"
+	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
 		staticruntime "On"
